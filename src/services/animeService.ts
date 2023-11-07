b41cb57a16c746ccc9618afad011dffc0d46ed91
Erik Garcia Tacas <erik.fisi16@gmail.com>
2023-11-06 22:19:22 -0500

    feat: add animeService

---
+++ src/services/animeService.ts
@@ -0,0 +1,31 @@
+import { BASE_URL_API } from "../utils/constants";
+
+class AnimeService {
+  static async getMovies({ search, type }: { search: string; type: string }) {
+    const response = await fetch(
+      `${BASE_URL_API}/anime?q=${search}&type=${type}`
+    );
+    const data = await response.json();
+    return data;
+  }
+
+  static async getCharacters({ mal_id }: { mal_id: number }) {
+    const response = await fetch(`${BASE_URL_API}/anime/${mal_id}/characters`);
+    const data = await response.json();
+    return data;
+  }
+
+  static async getFullCharacter({
+    character_mal_id,
+  }: {
+    character_mal_id: number;
+  }) {
+    const response = await fetch(
+      `${BASE_URL_API}/characters/${character_mal_id}/full`
+    );
+    const data = await response.json();
+    return data;
+  }
+}
+
+export default AnimeService;
---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+export const BASE_URL_API = "https://api.jikan.moe/v4";
+export const WORD_SEARCH = "one piece";
+export const TYPES = Object.freeze({
+  MOVIE: "Movie",
+});
+
+export const TITLES = Object.freeze({
+  LIST_MOVIES: "Lista de películas",
+});
