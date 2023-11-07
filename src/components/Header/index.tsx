0c8d537c8bbfb0bd0bc8482c901b25bcc527308f
Erik Garcia Tacas <erik.fisi16@gmail.com>
2023-11-06 20:28:18 -0500

    feat: add component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,17 @@
-const Header = () => {
-  return <div>index</div>;
+import { FC } from "react";
+
+interface Props {
+  title: string;
+}
+
+const Header: FC<Props> = ({ title }) => {
+  return (
+    <header className="bg-[#2e5d86] h-16">
+      <nav className="max-w-7xl px-4 mx-auto  h-full flex items-center">
+        <h1 className="text-white font-bold text-2xl">{title}</h1>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
